ac7c96ffca97279b92edc25cfc3a9fd9736ebad4
Shannqa <116514532+Shannqa@users.noreply.github.com>
2022-11-20 21:33:36 +0100

    Add a 5-round game function

--- js/javascript.js
+++ js/javascript.js
@@ -14,8 +14,8 @@ Algorithm:
 - Show a message announcing the winner.
 
 */
-let playerScore;
-let computerScore;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice () {
   let randomNumber = Math.floor(Math.random() * 3);
@@ -36,22 +36,47 @@ function getPlayerChoice() {
 function playRound(computerSelection, playerSelection) { 
 
   if (computerSelection === "rock" && playerSelection === "scissors") {
-    console.log("You lose! Rock beats scissors!" + playerSelection);
+    console.log("You lose! Rock beats scissors!");
+    computerScore += 1;
   } else if (computerSelection === "scissors" && playerSelection === "paper") {
     console.log("You lose! Scissors beat paper!");
-    } else if (computerSelection === "paper" && playerSelection === "rock") {
-      console.log("You lose! Paper beats beats rock!");
-    } else if (computerSelection === "paper" && playerSelection === "scissors") {
-      console.log("You win! Scissors beat paper!");
-    } else if (computerSelection === "scissors" && playerSelection === "rock") {
-      console.log("You win! Rock beats scissors!");
-    } else if (computerSelection === "rock" && playerSelection === "paper") {
-      console.log("You win! Paper beats rock!");
-    } else if (computerSelection === playerSelection) {
-      console.log("Tie!");
-    }
+    computerScore += 1;
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    console.log("You lose! Paper beats beats rock!");
+    computerScore += 1;
+  } else if (computerSelection === "paper" && playerSelection === "scissors") {
+    console.log("You win! Scissors beat paper!");
+    playerScore += 1;
+  } else if (computerSelection === "scissors" && playerSelection === "rock") {
+    console.log("You win! Rock beats scissors!");
+    playerScore += 1;
+  } else if (computerSelection === "rock" && playerSelection === "paper") {
+    console.log("You win! Paper beats rock!");
+    playerScore += 1;
+  } else if (computerSelection === playerSelection) {
+    console.log("Tie!");
+  }
 }
 
-let playerSelection = getPlayerChoice()
-let computerSelection = getComputerChoice();
-playRound(computerSelection, playerSelection);
+
+
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    let playerSelection = getPlayerChoice()
+    let computerSelection = getComputerChoice();
+    playRound(computerSelection, playerSelection); 
+  } 
+  
+  if (computerScore < playerScore) {
+    console.log ("Congratulations, you won!");
+  } else if (computerScore > playerScore) {
+    console.log ("Sorry, you lost!");
+  } else {
+    console.log ("It's a tie!");
+  }
+
+  console.log("Final result - Computer: " + computerScore + " Player: " + playerScore);
+ 
+}
+
+game();
